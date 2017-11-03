425c55b14c7ee106fd19c03eb326f6213cd45560
Shuhail <shuhail.alam@gmail.com>
2017-11-03 20:51:45 +0600

    Update and rename simplelogin.java to SimpleLogin.java

--- src/android/yapps/plugins/simplelogin/SimpleLogin.java
+++ src/android/yapps/plugins/simplelogin/SimpleLogin.java
@@ -4,7 +4,7 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class Simplelogin extends CordovaPlugin {
+public class SimpleLogin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
