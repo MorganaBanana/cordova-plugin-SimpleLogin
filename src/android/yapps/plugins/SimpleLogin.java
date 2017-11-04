79aebf744ea73cccab1d562f5a7fdfefe869cb0f
Shuhail <shuhail.alam@gmail.com>
2017-11-04 14:26:05 +0600

    Update SimpleLogin.java

--- src/android/yapps/plugins/SimpleLogin.java
+++ src/android/yapps/plugins/SimpleLogin.java
@@ -1,22 +1,34 @@
 package yapps.plugins;
 
-import org.apache.cordova.*;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.PluginResult.Status;
+import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.util.Log;
+
+import java.util.Date;
+
 public class SimpleLogin extends CordovaPlugin {
+  private static final String TAG = "user";
 
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    Log.d(TAG, "Initializing FeedReader");
+  }
 
-        if (action.equals("reed")) {
-
-            return false;
-
-        } else {
-            
-            return true;
-
-        }
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if(action.equals("echo")) {
+      String phrase = args.getString(0);
+      // Echo back the first argument
+      Log.d(TAG, phrase);
     }
+    return true;
+  }
+
 }
