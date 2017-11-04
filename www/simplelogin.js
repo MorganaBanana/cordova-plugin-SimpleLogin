73b136a64ca54f94cc12d79a296a5f3354c94e70
Shuhail <shuhail.alam@gmail.com>
2017-11-04 15:32:18 +0600

    Update simplelogin.js

--- www/simplelogin.js
+++ www/simplelogin.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
-var PLUGIN_NAME = 'user';
-var user = {
+var PLUGIN_NAME = 'SimpleLogin';
+var SimpleLogin = {
 	init: function(){
 		$('.login-form').hide();
 		$('#init').show();
@@ -24,13 +24,13 @@ var user = {
 				$('.login-msg').hide();
 				$('#WaitIcon').show();
 				var param = {name:name,email:email,secQ:secQ,secA:secA,pass:pass,gcm:localStorage.gcm,app_id:localStorage.app_id};
-				var response = user.verify('PLUGIN__userSignUp', param).done(function(response){
+				var response = SimpleLogin.verify('PLUGIN__userSignUp', param).done(function(response){
 					if(response!=0){
 						localStorage.name = name;
 						localStorage.user_id = response;
 						window.location.href = home_page;
 					}else{
-						$('.login-msg').html("Email already registered! <br><span onclick='user.init()'>Try Login?</span>");
+						$('.login-msg').html("Email already registered! <br><span onclick='SimpleLogin.init()'>Try Login?</span>");
 						$('.login-msg').show();
 						$('#WaitIcon').hide();
 					}
@@ -54,13 +54,13 @@ var user = {
 				$('.login-msg').hide();
 				$('#WaitIcon').show();
 				var param = {email:email,pass:pass,app_id:localStorage.app_id};
-				var response = user.verify('PLUGIN__userSignIn', param).done(function(response){
+				var response = SimpleLogin.verify('PLUGIN__userSignIn', param).done(function(response){
 					if(response!=0){
 						localStorage.name = response.name;
 						localStorage.user_id = response.user_id;
 						window.location.href = home_page;
 					}else{
-						$('.login-msg').html("Invalid credentials! <br><span onclick='user.resetPassword(1)'>Want to RESET?</span>");
+						$('.login-msg').html("Invalid credentials! <br><span onclick='SimpleLogin.resetPassword(1)'>Want to RESET?</span>");
 						$('.login-msg').show();
 						$('#WaitIcon').hide();
 					}
@@ -85,10 +85,10 @@ var user = {
 				$('.login-msg').hide();
 				$('#WaitIcon').show();
 				var param = {email:email,app_id:localStorage.app_id};
-				var response = user.verify('PLUGIN__checkEmail', param).done(function(response){
+				var response = SimpleLogin.verify('PLUGIN__checkEmail', param).done(function(response){
 					if(response!=0){
 						$('#WaitIcon').hide();
-						user.confirmReset(1,response.secQ,email);
+						SimpleLogin.confirmReset(1,response.secQ,email);
 					}else{
 						$('.login-msg').html("Email address is not registered");
 						$('.login-msg').show();
@@ -108,7 +108,7 @@ var user = {
 			$('.login-msg').html(secQ + '?');
 			$('.login-msg').show();
 			$('#confirmReset').show();
-			$('#confirmBtn').html('<input type="button" id="resetButton" value="Reset" onclick="user.confirmReset(0,\''+secQ+'\',\''+email+'\')" />');
+			$('#confirmBtn').html('<input type="button" id="resetButton" value="Reset" onclick="SimpleLogin.confirmReset(0,\''+secQ+'\',\''+email+'\')" />');
 			$('#back').show();
 		}else{
 			var secA = $('#secretAns').val();
@@ -118,7 +118,7 @@ var user = {
 				$('.login-msg').hide();
 				$('#WaitIcon').show();
 				var param = {email:email,pass:pass,secQ:secQ,secA:secA,app_id:localStorage.app_id};
-				var response = user.verify('PLUGIN__passwordReset', param).done(function(response){
+				var response = SimpleLogin.verify('PLUGIN__passwordReset', param).done(function(response){
 					if(response){
 						$('#WaitIcon').hide();
 						$('#secretAns').hide();
